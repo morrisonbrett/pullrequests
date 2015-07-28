530a844e01de3b555774ce5ad0733d03ed6f500c
Brett Morrison <b@brettmorrison.com>
2015-07-27 22:03:50 -0700

    Set up testing harness

---
+++ pullrequests_test.go
@@ -0,0 +1,15 @@
+//
+// Brett Morrison, July 2015
+//
+// Testing harness for pullrequest.go
+//
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLaunch(t *testing.T) {
+	fmt.Printf("Hello World\n")
+}
